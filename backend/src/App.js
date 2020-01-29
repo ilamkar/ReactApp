b6db2bbe517394aff00942b4250fc397dc010b8a
ilamkar <illum_dae@yahoo.com>
2020-01-29 15:02:34 -0600

    2nd test

--- backend/src/App.js
+++ backend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> relod please.
+          Edit <code>src/App.js</code> relod.
         </p>
         <a
           className="App-link"
