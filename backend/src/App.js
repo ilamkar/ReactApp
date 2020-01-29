5e173cf9703e8aa175ddb88fe2ce5d5db426b5aa
ilamkar <illum_dae@yahoo.com>
2020-01-29 14:56:33 -0600

    add text

--- backend/src/App.js
+++ backend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> relod.
+          Edit <code>src/App.js</code> relod please.
         </p>
         <a
           className="App-link"
